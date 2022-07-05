eaa28114a7b7af96d8e240dc71b0f69b02d7bb42
Stiven Herrera <stivenarleyherrera@gmail.com>
2022-07-04 22:07:13 -0500

    se agrega logica y funcion para trabajar la salida del struct con String

--- src/mispaquetes/mispaquetes.go
+++ src/mispaquetes/mispaquetes.go
@@ -11,11 +11,13 @@ type Computador struct {
 }
 
 //Funcion encargada de aumentar la ram segun el usuario lo requiera
-func (myPC *Computador) AumentarRam(vRam int) {
+func (myPC *Computador) AumentarRam(vRam int){
 	myPC.Ram = myPC.Ram + vRam
+}
+
+//Se agrega funcion que trabaja el output del struc Computador
+func (myPC Computador) String() string {
 	//se agrega logica para trabajar la salida de la funcion
 	//recordar %d para int y %s string
 	return fmt.Sprintf("Tengo %d GB de ram, %d GB de disco y es marca %s", myPC.Ram, myPC.Disco, myPC.Marca)
 }
-
-
