efdf756229efddcda9d46cfe0f5cd988239c8935
Stiven Herrera <stivenarleyherrera@gmail.com>
2022-07-04 23:43:56 -0500

    se agrega logica principal que ejecuta el calculo de areas para cuadrado y rectangulo

--- src/main.go
+++ src/main.go
@@ -1,54 +1,29 @@
 package main
 
-import "fmt"
+//importa carpeta o paquete donde estan nuestras herramientas
+import (
+	"curso_go-golang-main/src/mispaquetes"
+	"fmt"
+)
 
 //Interfaces por lo general se utilizan para centralizar los metodos o funciones que pueden estar en varios Structs
-//calcular area de cuadrado y rectangulo
-//SE AGREGA LA INTERFAZ QUE RECIBE LAS DOS FUNCIONES O METODOS "area"
-type figuras2D interface {
-	//se le indica que funcion va a recibir y que tipo de dato
-	area() float64
-}
-
-//struct o clase cuadrado privada
-type cuadrado struct {
-	base float64
-}
-
-//struct de area del rectangulo
-type rectangulo struct {
-	base   float64
-	altura float64
-}
-
-//Funcion que calcula el area del rectangulo, se referencia el struct cuadrado en la variable r TIENE EL MISMO NOMBRE QUE EL AREA DE CUADRADO -> area
-func (r rectangulo) area() float64 {
-	return r.base * r.altura
-}
-
-//Funcion que calcula el area del cuadrado, se referencia el struct cuadrado en la variable c TIENE EL MISMO NOMBRE QUE EL AREA DE CUADRADO -> area
-func (c cuadrado) area() float64 {
-	return c.base * c.base
-}
-
-//agregamos la funcion que estara encargada de procesar la interfaz figuras2D
-func calcular(f figuras2D) {
-	//le decimos que ejecute la funcion area()
-	fmt.Println("Area: ", f.area())
-}
+//Calcular Area de Cuadrado y rectangulo
+//SE AGREGA LA INTERFAZ QUE RECIBE LAS DOS FUNCIONES O METODOS "Area"
 
 //funcion principal que se ejecuta
 func main() {
-	//instanciamos structs en nueva variable y definimos que el valor base de la clase cuadrado va a ser 2
-	miCuadrado := cuadrado{base: 2}
-	miRectangulo := rectangulo{base: 2, altura: 4}
-	//Para ejecutar la interfaz para las dos funciones o metodos que comparten y retonran el mismo valor, se utiliza la funcion que procesa la interfaz calcular
-	//como parametro le pasamos la instancia del struct o clase, lo cual es mas optimo
-	calcular(miCuadrado)
-	calcular(miRectangulo)
-
+	//instanciamos los structs
+	pCuadrado := mispaquetes.Cuadrado{Base: 2}
+	pRectangulo := mispaquetes.Rectangulo{Base: 2, Altura: 4}
+	fmt.Println("Cuadrado: ", pCuadrado, "Rectangulo", pRectangulo)
+	//utilizamos el metodo publico Calcular para acceder a la interfaz privada Figuras2D la cual ejecuta los metodos compartidos Area() publicos de los paquetes cuadrado y rectangulo
+	fmt.Println("Cuadrado:")
+	mispaquetes.Calcular(pCuadrado)
+	fmt.Println("Rectangulo:")
+	mispaquetes.Calcular(pRectangulo)
 	//lista de interfaces
-	//go en las lsitas o slides o arrays necesita definir el parametro que va a contener esa lista, int o string etc, no varios tipos de datos en una misma
+	fmt.Println("Lista de interfaces:")
+	//En go en las lsitas o slides o arrays necesita definir el parametro que va a contener esa lista, int o string etc, no varios tipos de datos en una misma
 	//la manera de simular una lista con multiples tipos de datos es: []interface{} al definir el slides
 	miinterfaz := []interface{}{"Hola", 12, 4.2}
 	//imprimimos el slides con ... (toma cada uno de los elementos e imprime de manera individual)
