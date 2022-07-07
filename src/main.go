e75a1a23f6659b8bd62b3e982e38b0861bb918a4
Stiven Herrera <stivenarleyherrera@gmail.com>
2022-07-06 21:14:10 -0500

    se agrega clase de Channels

--- src/main.go
+++ src/main.go
@@ -1,37 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+//LOS CHANNELS PERMITEN COMPARTIR DATOS ENTRE LAS GOROUTINES, manejan de forma nativa la comunicacion.
+//los Channels solo se pueden manejar un tipo de dato. y este es asi mismo un una especie de conducto
 
-//TODO ESTE CODIGO USUALMENTE NO SE UTILIZA XD, MEJOR LOS CHANNEL
-func say(text string, wg *sync.WaitGroup) { // Gorutine
+import "fmt"
 
-	defer wg.Done() // Esta linea se va a ejecutar hasta el final de la funcion, y de esta forma libera el gorutine del WaitGroup
+func say(text string, c chan<- string) { // canal de entrada de datos
+	c <- text
+}
 
-	fmt.Println(text)
+func printChannelOutput(c <-chan string) { // canal de salida de datos
+	var output string
+	output = <-c
+	fmt.Println(output)
 }
 
 func main() {
-
-	var wg sync.WaitGroup // El paquete sync permite interacturar de forma primitiva con las gorutine. Variable que acomula un conjunto de gorutines y los va liberando poco a poco
-
+	// c := make(chan string) // dinamically accepts goroutines
+	c := make(chan string, 1) // one goroutine at time
 	fmt.Println("Hello")
+	go say("Bye", c)
 
-	wg.Add(1) // Indicamos que vamos a agregar 1 Gorutine al WaitGroup para que espere su ejecucion antes de que la gurutine base (main) muera, y así le de tiempo a la siguiente gorutine de ejecutarse
-
-	go say("world", &wg) // la palabra reservada go ejecutará la funcion de forma concurrente
-
-	wg.Wait() // Funcion del WaitGroup que sirve para decirle al gorutine principal (main) que espere hasta que todas las gorutine del WaitGroup finalicen, es decir, hasta que se ejecute 'defer wg.Done()' en cada una de las goroutines
-
-	go func(text string) { // Funciona anonima
-		fmt.Println(text)
-	}("Adios")
-
-	time.Sleep(time.Second * 1) // ! Funcion para que cuando llegue a esta linea espere el tiempo indicado (lo suficiente para que la Gorutine ejecute su funcion de forma concurrente)
-
-	// Nota: Para fines practicos se hace uso de la funcion Sleep(), pero en realidad NO es una buena practica, es mejor utilizar los WaitGroups
-
+	//fmt.Println(<-c)
+	printChannelOutput(c)
 }
